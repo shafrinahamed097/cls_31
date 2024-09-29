8ac8759523f25e6f9e1078ad940e55ee3af166e4
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-09-29 15:53:23 +0600

    Show all Users

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 
 use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
-    //
+    function demo1(){
+        $result = User::all();
+        return $result;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DemoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/demo1', [DemoController::class, 'demo1']);
